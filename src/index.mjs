c76d3153dfe179ed34ae47fbc9ea5b9eb2ec82d4
Vincent <vkammerer@gmail.com>
2021-05-15 10:21:59 +1200

    Parses data from CSV

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,13 @@
 import { readFileSync, writeFileSync } from "fs";
 import path from "path";
+import csvtojson from "csvtojson";
+
+const TEST_NAME = "LASC211_S1_test_2";
+
+// Ok when ran from root
 const __dirname = path.resolve("src");
 
-const TEST_NAME = "test2";
+const transpose = (m) => m[0].map((x, i) => m.map((x) => x[i]));
 
 const readTemplate = (name) =>
   readFileSync(path.resolve(__dirname, `../templates/${name}.xml`), {
@@ -18,9 +23,7 @@ const question_specie = readTemplate("question_specie");
 const question_common = readTemplate("question_common");
 const question_env = readTemplate("question_env");
 
-const output = container.replace(
-  "{{QUIZ}}",
-  `
+const questionsSetTemplate = () => `
 ${question_genus}
 
 ${question_specie}
@@ -28,22 +31,30 @@ ${question_specie}
 ${question_common}
 
 ${question_env}
-`
-);
+`;
 
-const rawData = JSON.parse(
-  readFileSync(path.resolve(__dirname, `../input/${TEST_NAME}.json`), {
-    encoding: "utf-8",
+const quiz = await csvtojson({ noheader: true, ignoreEmpty: true })
+  .fromFile(path.resolve(__dirname, `../input/${TEST_NAME}.csv`))
+  .then((users) => {
+    return transpose(users.map((u) => Object.values(u)));
   })
-);
+  .catch((err) => {
+    console.log(err);
+  });
 
-const data = {
-  ...rawData,
-  PLANT_NUMBER_FORMATTED: addZeroIfNeeded(rawData["PLANT_NUMBER"]),
-};
+const [fields, ...questionsSets] = quiz;
+const questionsXml = questionsSets
+  .map((set) =>
+    fields.reduce(
+      (acc, field, index) =>
+        acc
+          .replaceAll(`{{${field}}}`, set[index])
+          .replaceAll("{{PLANT_NUMBER_FORMATTED}}", addZeroIfNeeded(set[0])),
+      questionsSetTemplate()
+    )
+  )
+  .join("");
 
-const final = Object.keys(data).reduce((acc, expression) => {
-  return acc.replaceAll(`{{${expression}}}`, data[expression]);
-}, output);
+const output = container.replace("{{QUIZ}}", questionsXml);
 
-writeFileSync(path.resolve(__dirname, `../output/${TEST_NAME}.xml`), final);
+writeFileSync(path.resolve(__dirname, `../output/${TEST_NAME}.xml`), output);
